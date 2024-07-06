ee4a059413ea2445783a767dd57f00b76b1d1be6
Naman Narula <namankumarnarula@gmail.com>
2024-07-07 03:46:29 +0530

    Fixed failing test for single input

--- calculator.ts
+++ calculator.ts
@@ -1,5 +1,7 @@
-export function add(expression:string) : number|undefined {
-    if(expression.length === 0){
-        return 0;
-    }
+export function add(expression: string): number | undefined {
+  if (expression.length === 0) {
+    return 0;
+  } else if (expression.length === 1) {
+    return Number.parseInt(expression);
+  }
 }
