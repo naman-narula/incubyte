d5ed05ba363f4c3e1d752fb3e4fea3a961a8f244
Naman Narula <namankumarnarula@gmail.com>
2024-07-07 03:48:28 +0530

    Fixed failing test for addition of two numbers

--- calculator.ts
+++ calculator.ts
@@ -4,4 +4,10 @@ export function add(expression: string): number | undefined {
   } else if (expression.length === 1) {
     return Number.parseInt(expression);
   }
+
+  let operands = expression.split(',');
+
+  if (operands.length === 2) {
+    return Number.parseInt(operands[0]) + Number(operands[1]);
+  }
 }
