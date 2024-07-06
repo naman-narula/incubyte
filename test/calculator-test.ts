d96dd2e51708dc80d6c1a76d254da53d7dd9e21c
Naman Narula <namankumarnarula@gmail.com>
2024-07-07 03:46:11 +0530

    Added test for single input

--- test/calculator-test.ts
+++ test/calculator-test.ts
@@ -6,4 +6,7 @@ describe('Tests for addition operation of calculator', function () {
     it('should return 0 for empty string', function () {
         expect(add('')).equals(0);
     });
+    it("should return same number on single input", function () {
+        expect(add("1")).equals(1);
+    })
 });
