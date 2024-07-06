d02ec493eb928e282aebc07d78736bbb41da0f59
Naman Narula <namankumarnarula@gmail.com>
2024-07-07 04:01:21 +0530

    Fixed failing test for different delimiter

--- calculator.ts
+++ calculator.ts
@@ -2,8 +2,16 @@ export function add(expression: string): number {
   if (expression.length === 0) {
     return 0;
   }
-
-  const operands = expression.split(/[,\n]/).map((ele) => Number.parseInt(ele));
+  let delimiter = /[,\n]/;
+  if (expression.startsWith('//')) {
+    //delimiter could be a * which is special character. Hence \\
+    delimiter = new RegExp(`\\${expression[2]}`);
+    // expression starts after \n
+    expression = expression.substring(4);
+  }
+  const operands = expression
+    .split(delimiter)
+    .map((ele) => Number.parseInt(ele));
   return operands.reduce((sum, operand) => {
     return sum + operand;
   }, 0);
