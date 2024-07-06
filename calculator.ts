ba739ab04bbedcfe6dcd6e2d3d1d1752091f6677
Naman Narula <namankumarnarula@gmail.com>
2024-07-07 03:49:57 +0530

    Fixed failing test for addition of unknown amount of numbers

--- calculator.ts
+++ calculator.ts
@@ -1,13 +1,10 @@
-export function add(expression: string): number | undefined {
+export function add(expression: string): number {
   if (expression.length === 0) {
     return 0;
-  } else if (expression.length === 1) {
-    return Number.parseInt(expression);
   }
 
-  let operands = expression.split(',');
-
-  if (operands.length === 2) {
-    return Number.parseInt(operands[0]) + Number(operands[1]);
-  }
+  const operands = expression.split(',').map((ele) => Number.parseInt(ele));
+  return operands.reduce((sum, operand) => {
+    return sum + operand;
+  }, 0);
 }
